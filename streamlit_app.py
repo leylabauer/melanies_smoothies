2ac0cbe5bd7be2a30a718e90094b4aea49e75450
Leyla Bauer <52169615+leylabauer@users.noreply.github.com>
2024-09-23 14:36:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
-
-
 cnx = st.connection("snowflake")
 session = cnx.session()
 
@@ -51,3 +46,10 @@ if ingredients_list:
     # if ingredients_string:
     #     session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, Easy ' + name_on_order, icon="✅")
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
+
